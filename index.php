31026717d6aab0aaef8530dae57ea5ad92d9cc74
filtah <filtah@outlook.com>
2018-11-14 18:27:25 +0100

    index and single changes

--- index.php
+++ index.php
@@ -2,4 +2,34 @@
 
 <pre>[INDEX]</pre>
 
+<?php setup_postdata( $post = get_queried_object_id() ); ?>
+<pre>	[TITLE]</pre>
+<div style="margin-left: 8em;">
+	<h1><?php the_title(); ?></h1>
+</div>
+
+<pre>	[CONTENT]</pre>
+<div style="margin-left: 8em;">
+	<?php the_content(); ?>
+</div>
+<?php wp_reset_postdata(); ?>
+
+
+<pre>	[LOOP]</pre>
+<?php while ( have_posts() ) : the_post(); ?>
+
+	<pre>		[TITLE]</pre>
+	<div style="margin-left: 16em;">
+		<h1><?php the_title(); ?></h1>
+	</div>
+
+	<pre>		[CONTENT]</pre>
+	<div style="margin-left: 16em;">
+		<?php the_content(); ?>
+	</div>
+
+	<div style="margin-left: 8em; max-width: 50em;"><hr></div>
+
+<?php endwhile; ?>
+
 <?php get_footer(); ?>
